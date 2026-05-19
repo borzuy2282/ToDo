41170240c0ee911a85fd77eb18a96eeb90042f08
Tymofii <tim.barzashvili@icloud.com>
2026-05-19 20:47:57 +0200

    Todo entity
    Added todo entity, added to db and configuered.

---
+++ src/main/java/com/springboot/todo/entity/Todo.java
@@ -0,0 +1,28 @@
+package com.springboot.todo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "todo")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    private boolean done;
+}

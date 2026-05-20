6bd05a6cdaa7b0daba5cb35f5b388d54f0b99dca
Tymofii <tim.barzashvili@icloud.com>
2026-05-20 22:47:52 +0200

    User and Role repositories
    Added user and role repositories. Provided few useful methods to user repo.

---
+++ src/main/java/com/springboot/todo/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.todo.repository;
+
+import com.springboot.todo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/springboot/todo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.springboot.todo.repository;
+
+import com.springboot.todo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Boolean existsByEmail(String email);
+}

856050ef350b930b28d17af280dddd0b27244e87
Tymofii <tim.barzashvili@icloud.com>
2026-05-21 19:46:54 +0200

    Jwt token provider
    Added jwt token provider with a bunch of methods that will be used to ensure jwt logic in the application.

---
+++ src/main/java/com/springboot/todo/security/JwtTokenProvider.java
@@ -0,0 +1,54 @@
+package com.springboot.todo.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+    @Value("${app.jwt.expiration-milli}")
+    private long jwtExpirationMilli;
+
+    public String generateToken(Authentication authentication){
+        String login = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationMilli);
+
+        return Jwts.builder()
+                .subject(login)
+                .issuedAt(currentDate)
+                .expiration(expireDate)
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUsernameFromToken(String token){
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token){
+        Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parse(token);
+        return true;
+    }
+}
